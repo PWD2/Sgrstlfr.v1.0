4995c299facc3f5a9b96a35ade69e745dbe83e6a
PWD2 <81218657+PWD2@users.noreply.github.com>
2023-03-30 15:17:39 +0800

    Update run_test599.sh

--- run_test599.sh
+++ run_test599.sh
@@ -3,6 +3,5 @@ perl /hwfssz1/ST_BIOCHEM/P18Z10200N0255/PMO/F14ZQSYJSY1726/pangwending/Sgrstlfr/
 -rfcvg /hwfssz1/ST_BIOCHEM/P18Z10200N0255/PMO/F14ZQSYJSY1726/pangwending/SCR_Syn09RD/refer_index/JS94.coverage.depthsingle \
 -fq1 /hwfssz1/ST_BIOCHEM/P18Z10200N0255/PMO/F14ZQSYJSY1726/pangwending/New_Data/yeast_L01/JS599/CL100176035_L01_1_1.fq.gz \
 -fq2 /hwfssz1/ST_BIOCHEM/P18Z10200N0255/PMO/F14ZQSYJSY1726/pangwending/New_Data/yeast_L01/JS599/CL100176035_L01_1_2.fq.gz \
--lxpreg /hwfssz1/ST_BIOCHEM/P18Z10200N0255/PMO/F14ZQSYJSY1726/pangwending/SCR_Syn09RD/refer_index/IXR_BACseq.loxpreg \
 -n test599 -o /hwfssz1/ST_BIOCHEM/P18Z10200N0255/PMO/F14ZQSYJSY1726/pangwending/New_Data/yeast_L01/JS599 \
--chrtype cycle -chrid IXR_BACseq  -minread 100 -mdep 200 
+-chrtype cycle -chrid IXR_BACseq 
